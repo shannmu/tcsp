b821a9986f45540b17831f8b710f73a26b82b5a9
shannmu <shanmu1901@gmail.com>
2024-11-04 13:57:48 +0800

    fix: update uart_server example

--- examples/uart_server.rs
+++ examples/uart_server.rs
@@ -13,9 +13,9 @@ use tokio::time::timeout;
 #[derive(Parser, Debug)]
 #[command(about, long_about = None)]
 struct Args {
-    #[arg(long, required = true)]
+    #[arg(long, required = true, default_value = "/dev/ttyAMA1")]
     device_name: String,
-    #[arg(long, required = true)]
+    #[arg(long, required = true, default_value = "0x85")]
     device_id: u8,
 }
 
@@ -33,22 +33,22 @@ async fn main() {
     );
 
     let socket = ZeromqSocket::new();
-    // timeout(
-    //     Duration::from_secs(2),
-    //     socket.connect("tcp://127.0.0.1:5555"),
-    // )
-    // .await
-    // .expect("Connection timeout")
-    // .expect("Failed to connect");
+    timeout(
+        Duration::from_secs(2),
+        socket.connect("tcp://127.0.0.1:5555"),
+    )
+    .await
+    .expect("Connection timeout")
+    .expect("Failed to connect");
     #[allow(clippy::unwrap_used)]
     let adaptor = Uart::new(device_name.as_str(), 115200, device_id).await;
     let server = TcspServerBuilder::new_uart(adaptor)
-        // .with_application(Arc::new(TeleMetry::new(socket.clone())))
-        // .with_application(Arc::new(EchoCommand {}))
-        // .with_application(Arc::new(TimeSync::new(socket.clone())))
-        // .with_application(Arc::new(Reboot {}))
-        // .with_application(Arc::new(UdpBackup::new(socket.clone())))
-        // .with_application(Arc::new(ResetNetwork {}))
+        .with_application(Arc::new(TeleMetry::new(socket.clone())))
+        .with_application(Arc::new(EchoCommand {}))
+        .with_application(Arc::new(TimeSync::new(socket.clone())))
+        .with_application(Arc::new(Reboot {}))
+        .with_application(Arc::new(UdpBackup::new(socket.clone())))
+        .with_application(Arc::new(ResetNetwork {}))
         .with_application(Arc::new(UploadCommand::new(socket.clone())))
         .with_application(Arc::new(DownloadCommand::new(socket.clone())))
         .build();
